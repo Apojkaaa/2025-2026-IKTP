054d6b30c6449c64275e0f4bf2ce9de0d88a2d88
Soltész Apor <soltesz.apor@ckik.loc>
2025-11-21 10:18:51 +0100

    mentés

---
+++ SoltészApor.py
@@ -0,0 +1,16 @@
+import random
+import math
+#1/a feladat
+a = random.randint(10,99)
+#1/b feladat
+b = random.randint(100,999)
+for szam in range (a):
+    print(szam, end=" ")
+#1/c feladat
+c = szam/3
+print(c)
+#1/d feladat
+d = 300 
+print(szam)
+#2/a feladat
+
