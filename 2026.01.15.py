c3d16b36ce21e506c817f697b74eddf2ea9ee895
Soltész Apor <soltesz.apor@ckik.loc>
2026-01-16 11:32:25 +0100

    mentés

--- 2026.01.15.py
+++ 2026.01.15.py
@@ -1,5 +1,5 @@
+import random
 """
-
 Függvények
 (Chratch blokkok)
 
@@ -10,7 +10,6 @@ def fuggvenyNev():
 
 fuggvenynev() #függvény meghívása
 """
-
 # visszatérés érték nélküli függvények - eljárás
 #általában iyratás nélkül 
 #osszeadas függvény definiálása 
@@ -18,7 +17,6 @@ def osszeadas():
     a = 12
     b =17
     print(a+b)
-
 #összeadas klső értéktől függően paraméteren keresztül
 def osszeadasParam(a,b):
     c = a + b
@@ -26,16 +24,49 @@ def osszeadasParam(a,b):
 #összeadas függvény meghívása
 osszeadas()
 osszeadasParam(13,19)
-
 # visszatéréssel rnedelkező függvények 
 def kettoAtizediken():
     # a = math.pow(2,10)
     a = 2**10
     return a 
 kettoAtizediken()
-
 def osszeadasVisszateressel(a,b):
     c = a + b 
     return c 
-
 print(osszeadasVisszateressel(13,17))
+
+def veletlenszamkiiratas(db):
+    for i in range(0,db,1):
+        print(random.randint(1,9),end=" ")
+    print()
+veletlenszamkiiratas(5)
+
+def szovegvisszafele(szoveg):
+    for index in range(len(szoveg)-1,-1,-1):
+        print(szoveg[index], end="")
+    print()
+szovegvisszafele("kalapács")
+
+def szovegvisszafelefv(szoveg):
+    visszaszoveg = ""
+    for index in range(len(szoveg)-1,-1,-1):
+        visszaszoveg += szoveg[index]
+    return visszaszoveg
+    
+
+print(szovegvisszafelefv("kalapács"))
+
+#irjon egy fv-t ami egy szórol eldönti hogy palindrom-e és vissza adja válaszul 
+def palindromszoveg(palszoveg):
+    # i = 0
+    # while(i<len(palszoveg)//2 and palszoveg[i] == palszoveg[len(palszoveg)-1-i]):
+    #     i+=1
+    # if(i>len(palszoveg)//2):
+    #     return "palindrom"
+    # else:
+    #     return "Nem palindrom"
+    if (palszoveg == szovegvisszafelefv(palszoveg)):
+        return True
+    else:
+        return False
+print(palindromszoveg("cig"))
---
+++ 2026.01.16.py
@@ -0,0 +1,18 @@
+import random
+#keszitsen egy fuggvenyt ami egy darabaszámtol fugg es vissza ad egy feltoltott listat [-10,50] közötti számokkal
+
+def listafeltolt(db):
+    lista = []
+    for i in range(0,db,1):
+        szam = random.randint(-10,50)
+        lista.append(szam)
+    return lista
+print(listafeltolt(5))
+# készítsen egy függvényt ami bármilyen lista elemszámait megvizsgálva visszadja hogy mennyi poz szám van
+
+def pozitivDb(szamokLista):
+    darab = 0
+    for i in range(0,len(szamokLista),1):
+        if (szamokLista[i]>1):
+            darab +=1
+    return darab
