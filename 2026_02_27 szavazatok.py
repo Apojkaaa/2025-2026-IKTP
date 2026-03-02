4469210510989b8ae6eb7c87e345525fa19da46b
Soltész Apor <soltesz.apor@ckik.loc>
2026-03-02 11:30:26 +0100

    mentés

--- 2026_02_27 szavazatok.py
+++ 2026_02_27 szavazatok.py
@@ -3,48 +3,90 @@ def listaFeltoltes():
     lista = []
     db = int(input())
     for i in range(db):
-        st = input().split(" ")
-        lista.append((int(st[0]),int(st[1]),st[2],st[3],st[4]))
+        st = input().split(' ')
+        lista.append((int(st[0]), int(st[1]), st[2], st[3],st[4]))
     return lista
 
-def feladat4(adatok):
-    szavazatokSzama = osszesSzavazat(adatok)
-    mindenki = 12345
-def osszegzes(adatok):
-    osszeg = 0
-    for i in range(0,len(adatok),1):
-        osszeg += adatok[i]
-    return osszeg
-def kereses(lista,vn,kn):
+def kereses(lista, vn, kn):
     i = 0
-    while(i<len(lista) and not (lista[i][2] == vn and lista[i][3] == kn)):
-        i += 1
+    #while(i<len(lista) and (lista[i][2]!= vn or lista[i][3]!= kn)):
+    while(i<len(lista) and not (lista[i][2]== vn and lista[i][3]== kn)):
+        i+=1
     if(i<len(lista)):
         return i
     else:
         return -1
-    
 
 def feladat3(adatok):
-    vezeteknev = input("Adja meg a vezeteknevet: ")
+    vezeteknev = input("Adja meg a vezetéknevet: ")
     keresztnev = input("Adja meg a keresztnevet: ")
-
+    #nev = input("Adjam egy teljes nevét szóközzel elválasztva: ").split(" ")
+    # nev[0] - vezeteknev, nev[1] - keresztnev
     index = kereses(adatok,vezeteknev,keresztnev)
-    if (index >= 0):
+    if(index >= 0 ):
         print(adatok[index][1])
     else:
-        print("Ilyen nevű képviselőjelölt nincs a listában")
+        print("Ilyen nevű képviselőjelölt nem szerepel a nyilvántartásban!")
+
+def osszesSzavazat(adatok):
+    osszeg = 0
+    for i in range(len(adatok)):
+        osszeg += adatok[i][1]
+    return osszeg
 
 
-    
+def feladat4(adatok):
 
+    szavazatokSzama = osszesSzavazat(adatok)
+    mindenki = 12345
+    szazalek = szavazatokSzama / mindenki * 100
+    print(f"A választáson {szavazatokSzama} állampolgár, a jogosultak {round(szazalek,2)}%-a vett részt.")
+    # A választáson 5001 állampolgár, a jogosultak 40,51%-a vett részt.
+
+def feladat5(adatok):
+    gyep = partDarab(adatok,"GYEP")
+    hep = partDarab(adatok,"HEP")
+    tisz = partDarab(adatok,"TISZ") 
+    zep = partDarab(adatok,"ZEP")
+    fugg = partDarab(adatok,"-")
+    print("Gyümölcsevők pártja= ",gyep)
+    print("Húsevők pártja= ",hep)
+    print("Tejivók partja= ",tisz)
+    print("zöldségevők partja= ",zep)
+    print("fuggetlen part= ",fugg)
+def partDarab(adatok,part):
+    db = 0
+    for i in range(len(adatok)):
+        if(adatok[i][4] == part):
+            db += 1
+    return db
+def maximumSzavazatok(adatok):
+    maxe=adatok[0][1]
+    for i in range(len(adatok)):
+        if (adatok[i][1]>maxe):
+            maxe = adatok[i][1]
+    return maxe
+def feladat6(adatok):
+    maxe =maximumSzavazatok(adatok)
+    for i in range(len(adatok)):
+        if(adatok[i][1] == maxe):
+            print(f{"adatok[i][2]} {adatok[i][3] } -{adatok [1][4]} ")
 def main():
     adatok = listaFeltoltes()
-    print(adatok)
+    #print(adatok)
 
-    print("A helyhatósági választáson" ,len(adatok),"képviselőjelölt indult.")
+    # 2. feladat
+    print("A helyhatósági választáson",len(adatok),"képviselőjelölt indult.")
 
+    # 3. feladat
     feladat3(adatok)
 
+    # 4. feladat
+    feladat4(adatok)
 
+    # 5. feladat
+    feladat5(adatok)
+
+    feladat6(adatok)
+    # aminosavas feladat
 main()
